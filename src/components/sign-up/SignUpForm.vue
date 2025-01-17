255a0333a73edcd9fb422c9daa446ebba7302391
Henry Carmenate <henrycarmenateg@gmail.com>
2025-01-16 23:16:05 -0500

    Added validation to register form

--- src/components/sign-up/SignUpForm.vue
+++ src/components/sign-up/SignUpForm.vue
@@ -1,22 +1,58 @@
-<script setup lang="ts">
+<script setup>
+import { Form } from 'vee-validate'
 import TextField from '@/components/UI/form/TextField.vue'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 import EmailField from '@/components/UI/form/EmailField.vue'
 import PasswordField from '@/components/UI/form/PasswordField.vue'
-
+import { toTypedSchema } from '@vee-validate/zod'
+import * as zod from 'zod'
 
 const localState = reactive({
   firstName: '',
   lastName: '',
   email: '',
-  password: ''
+  password: '',
+  sending: false
 })
+
+const registerValidationSchema = computed(() => {
+  return toTypedSchema(
+    zod.object({
+      firstName: zod.string().min(1, {message: 'First Name is required'}),
+      lastName: zod.string().min(1, {message: 'Last Name is required'}),
+      email: zod.string().email({message: 'Email is required'}),
+      password: zod.string({message: 'Password is required'}).min(8, {message: 'Password must have at least 8 characters long'})
+    })
+  )
+})
+
+const onSubmit = async () => {
+  try {
+    localState.sending = true
+
+    // Request to register the new user in cwm
+
+  } catch(e) {
+    console.log('Ops Something happens!', e)
+  } finally {
+    localState.sending = false
+  }
+}
+
+const onInvalidSubmit = (errors) => {
+  console.log(errors)
+}
+
 </script>
 
 <template>
   <div class="sign-up-form w-1/2 bg-gray-800 rounded p-10">
     <h3 class="font-semibold text-xl mb-10 text-white">Let's Start!</h3>
-    <form >
+    <Form
+      :validation-schema="registerValidationSchema"
+      @submit="onSubmit"
+      @invalid-submit="onInvalidSubmit"
+    >
       <div class="mb-4">
         <TextField
           :placeholder="'Enter your first name'"
@@ -81,7 +117,7 @@ const localState = reactive({
           Register
         </button>
       </div>
-    </form>
+    </Form>
   </div>
 </template>
 
