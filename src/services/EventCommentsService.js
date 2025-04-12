7ace62390d3240015f722c9cbf390ea35ca9fe6b
Henry Carmenate <henrycarmenateg@gmail.com>
2025-04-11 21:57:30 -0400

    Refactor comment addition logic and enhance error logging.
    Streamlined the addComment method by simplifying nested promises into async/await format for better readability. Added detailed request parameter logging for debugging and improved error logging with additional context about the error details.

--- src/services/EventCommentsService.js
+++ src/services/EventCommentsService.js
@@ -51,25 +51,31 @@ class EventCommentsService {
 
   async addComment({ eventId, userId, origin, comment }) {
     try {
-      return CWM_API.get('sanctum/csrf-cookie', {
+      // Log para debug
+      console.log('Request parameters:', { eventId, userId, origin, comment });
+
+      await CWM_API.get('sanctum/csrf-cookie', {
         baseURL: import.meta.env.VITE_API_URL,
-      }).then(async () => {
-        return await CWM_API.post(`event/${eventId}/comments`, {
-          userId,
-          origin,
-          comment
-        })
-      })
+      });
+
+      const response = await CWM_API.post(`event/${eventId}/comments`, {
+        userId,
+        origin,
+        comment
+      });
+
+      return response;
     } catch (error) {
+      // Log detallado del error
       console.error('Error details:', {
         message: error.message,
         response: error.response?.data,
         status: error.response?.status
       });
       throw error;
-
     }
   }
+
 
   async loadComments(eventId) {
     return await CWM_API.get(`event/${eventId}/comments`)
