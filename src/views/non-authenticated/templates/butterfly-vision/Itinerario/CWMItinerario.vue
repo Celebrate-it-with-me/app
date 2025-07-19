2191e0e6cc6be2cc9a4cee479c06c0173bd0e5e2
Henry Carmenate <henrycarmenateg@gmail.com>
2025-07-19 14:58:36 -0400

    Refine Itinerario styles: improve timeline image responsiveness, simplify classes, and update background handling for better layout consistency.

--- src/views/non-authenticated/templates/butterfly-vision/Itinerario/CWMItinerario.vue
+++ src/views/non-authenticated/templates/butterfly-vision/Itinerario/CWMItinerario.vue
@@ -4,129 +4,119 @@ import CeremonyGif from '@/assets/images/Itinerario/ceremony.gif'
 import FoodGif from '@/assets/images/Itinerario/food-delivery.gif'
 import DanceGif from '@/assets/images/Itinerario/dance.gif'
 import DanceFloor from '@/assets/images/Itinerario/dance-floor.gif'
+
+
 </script>
 
 <template>
   <div
     id="sectionItinerario"
-    class="relative overflow-hidden h-[100vh] w-full"
+    class="itinerario-container bg-gray-200 mx-auto flex justify-center w-full h-screen min-h-screen
+           bg-pink-300 p-2 md:p-8"
   >
-    <div class="absolute inset-0 w-full h-full">
-      <img
-        src="@/assets/images/img/itinerario_bg_2.jpg"
-        alt="Background"
-        class="object-cover w-full h-full transform translate-z-0"
-        style="transform: translateZ(0);"
-      />
-    </div>
     <div
-      class="itinerario-container relative z-10 mx-auto flex justify-center w-full h-screen min-h-screen
-             bg-pink-300 p-2 md:p-8"
+      class="timeline-container relative w-full md:w-3/4 wrap overflow-hidden py-10 px-2 bg-gray-200/40 rounded-lg"
     >
+      <h2 class="text-gray-900 text-center mb-2 font-bold text-5xl font-gvibes">Itinerario</h2>
       <div
-        class="timeline-container relative w-full md:w-3/4 wrap overflow-hidden py-10 px-2 bg-gray-200/40 rounded-lg"
-      >
-        <h2 class="text-gray-900 text-center mb-2 font-bold text-5xl font-gvibes">Itinerario</h2>
-        <div
-          class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
-          style="left: 50%"
-        ></div>
+        class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
+        style="left: 50%"
+      ></div>
+      <!-- right timeline -->
+      <div class="flex flex-col justify-between h-full">
+        <div class="mb-8 flex justify-between items-center w-full right-timeline">
+          <div class="order-1 w-5/12 flex justify-center items-center">
+            <img
+              :src="MargaritaGif"
+              alt="Margarita"
+              class="h-auto rounded-lg w-24 h-24"
+            />
+          </div>
+          <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+            <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
+          </div>
+          <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
+            <h3 class="font-bold text-[#111827] text-md">Cocktail</h3>
+            <p class="text-sm font-medium leading-snug tracking-wide text-[#111827] text-opacity-100">
+              7:00pm-8:00pm
+            </p>
+          </div>
+        </div>
+
+        <!-- left timeline -->
+        <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+          <div class="order-1 w-5/12 flex justify-center items-center">
+            <img
+              :src="CeremonyGif"
+              alt="Margarita"
+              class="w-24 h-24 h-auto rounded-lg"
+            />
+          </div>
+          <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+            <h1 class="mx-auto text-white font-semibold text-lg">2</h1>
+          </div>
+          <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
+            <h3 class="font-bold text-[#111827] text-xl">Ceremonia</h3>
+            <p class="text-sm font-medium leading-snug tracking-wide text-[#111827] text-opacity-100">
+              8:00pm-9:30pm
+            </p>
+          </div>
+        </div>
+
         <!-- right timeline -->
-        <div class="flex flex-col justify-between h-full">
-          <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12 flex justify-center items-center">
-              <img
-                :src="MargaritaGif"
-                alt="Margarita"
-                class="object-contain max-w-[6rem] z-10 rounded-lg w-24 h-24"
-              />
-            </div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
-            </div>
-            <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
-              <h3 class="font-bold text-[#111827] text-md">Cocktail</h3>
-              <p class="text-sm font-medium leading-snug tracking-wide text-[#111827] text-opacity-100">
-                7:00pm-8:00pm
-              </p>
-            </div>
+        <div class="mb-8 flex justify-between items-center w-full right-timeline">
+          <div class="order-1 w-5/12 flex justify-center items-center">
+            <img
+              :src="FoodGif"
+              alt="Margarita"
+              class="w-24 h-24 h-auto rounded-lg"
+            />
           </div>
-
-          <!-- left timeline -->
-          <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1 w-5/12 flex justify-center items-center">
-              <img
-                :src="CeremonyGif"
-                alt="Ceremony"
-                class="object-contain max-w-[6rem] z-10 rounded-lg w-24 h-24"
-              />
-            </div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 class="mx-auto text-white font-semibold text-lg">2</h1>
-            </div>
-            <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
-              <h3 class="font-bold text-[#111827] text-xl">Ceremonia</h3>
-              <p class="text-sm font-medium leading-snug tracking-wide text-[#111827] text-opacity-100">
-                8:00pm-9:30pm
-              </p>
-            </div>
+          <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+            <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
           </div>
-
-          <!-- right timeline -->
-          <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12 flex justify-center items-center">
-              <img
-                :src="FoodGif"
-                alt="Food"
-                class="object-contain max-w-[6rem] z-10 rounded-lg w-24 h-24"
-              />
-            </div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
-            </div>
-            <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
-              <h3 class="font-bold text-[#111827] text-lg">Cena</h3>
-              <p class="text-sm leading-snug tracking-wide text-[#111827] text-opacity-100">
-                9:30pm-10:30pm
-              </p>
-            </div>
+          <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
+            <h3 class="font-bold text-[#111827] text-lg">Cena</h3>
+            <p class="text-sm leading-snug tracking-wide text-[#111827] text-opacity-100">
+              9:30pm-10:30pm
+            </p>
           </div>
+        </div>
 
-          <!-- left timeline -->
-          <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1 w-5/12 flex justify-center items-center">
-              <img
-                :src="DanceGif"
-                alt="Dance"
-                class="object-contain max-w-[6rem] z-10 rounded-lg w-24 h-24"
-              />
-            </div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 class="mx-auto text-white font-semibold text-lg">4</h1>
-            </div>
-            <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
-              <h3 class="font-bold text-[#111827] text-lg">Baile Sorpresa</h3>
-              <p class="text-sm font-medium leading-snug tracking-wide text-[#111827] text-opacity-100">
-                10:30pm-11:00pm
-              </p>
-            </div>
+        <!-- left timeline -->
+        <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+          <div class="order-1 w-5/12 flex justify-center items-center">
+            <img
+              :src="DanceGif"
+              alt="Margarita"
+              class="w-24 h-24 h-auto rounded-lg"
+            />
           </div>
+          <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+            <h1 class="mx-auto text-white font-semibold text-lg">4</h1>
+          </div>
+          <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
+            <h3 class="font-bold text-[#111827] text-lg">Baile Sorpresa</h3>
+            <p class="text-sm font-medium leading-snug tracking-wide text-[#111827] text-opacity-100">
+              10:30pm-11:00pm
+            </p>
+          </div>
+        </div>
 
-          <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12 flex justify-center items-center">
-              <img
-                :src="DanceFloor"
-                alt="Dance Floor"
-                class="object-contain max-w-[6rem] z-10 rounded-lg w-24 h-24"
-              />
-            </div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 class="mx-auto font-semibold text-lg text-white">5</h1>
-            </div>
-            <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
-              <h3 class="font-bold text-[#111827] text-lg">Hora Loca</h3>
-              <p class="text-sm leading-snug tracking-wide text-[#111827] text-opacity-100">11:00pm</p>
-            </div>
+        <div class="mb-8 flex justify-between items-center w-full right-timeline">
+          <div class="order-1 w-5/12 flex justify-center items-center">
+            <img
+              :src="DanceFloor"
+              alt="Dance Floor"
+              class="w-24 h-24 h-auto rounded-lg"
+            />
+          </div>
+          <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+            <h1 class="mx-auto font-semibold text-lg text-white">5</h1>
+          </div>
+          <div class="order-1 bg-[#baa7fb] rounded-lg shadow-xl w-5/12 px-2 py-2">
+            <h3 class="font-bold text-[#111827] text-lg">Hora Loca</h3>
+            <p class="text-sm leading-snug tracking-wide text-[#111827] text-opacity-100">11:00pm</p>
           </div>
         </div>
       </div>
@@ -136,14 +126,28 @@ import DanceFloor from '@/assets/images/Itinerario/dance-floor.gif'
 
 <style scoped>
 .itinerario-container {
+  background-image: url('@/assets/images/img/itinerario_bg_2.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
   min-height: 100vh;
   width: 100%;
   position: relative;
 }
 
+@media screen and (min-width: 768px) {
+  .itinerario-container {
+    background-size: 100% auto;
+    background-position: center;
+  }
+}
+
 @media screen and (min-width: 1024px) {
-  #sectionItinerario .absolute img {
-    content: url('@/assets/images/img/itinerario_bg_large.jpg');
+  .itinerario-container {
+    background-image: url('@/assets/images/img/itinerario_bg_large.jpg');
+    background-size: cover;
+    background-position: center;
   }
 }
 
